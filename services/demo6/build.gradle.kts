de06a6600953f99b6a72f8db2d2236aad91c7e80
Cristian Aguilera <crcaguilerapo@unal.edu.co>
2023-09-18 17:06:40 -0500

    5 - Configurate Sonar from Gradle (#5)
    * remove scanner from demo3
    
    * config sonar from gradle
    
    * fix pipeline
    
    * add property

---
+++ services/demo6/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("org.sonarqube") version "4.3.1.3277"
+
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "crcaguilerapo_demo6")
+        property("sonar.organization", "crcaguilerapo")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.qualitygate.wait", "true")
+    }
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
