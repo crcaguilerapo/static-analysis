8f4f31ff281adaa8a03d512e85d9fec1fb8bd3bc
criscapunal <crcaguilerapo@unal.edu.co>
2024-01-20 18:48:04 -0500

    add code smell

--- services/demo2/demo2/main.py
+++ services/demo2/demo2/main.py
@@ -1,1 +1,18 @@
+def calcular_area_cuadrado(lado):
+    return lado * lado
+
+def calcular_perimetro_cuadrado(lado):
+    return 4 * lado
+
+def calcular_area_rectangulo(base, altura):
+    return base * altura
+
+def calcular_perimetro_rectangulo(base, altura):
+    return 2 * (base + altura)
+
+def unused_variable():
+    a = 10
+    # 'a' no se utiliza en el resto del código
+    return 5
+
 print("hello world!!")
