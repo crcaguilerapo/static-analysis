fd8f232b2537c7389557a6c6ab5501d1ccb9c802
Cristian Aguilera <crcaguilerapo@unal.edu.co>
2024-01-22 10:42:02 -0500

    8 - add changes (#8)
    * add changes
    
    * add code smell
    
    * remove exclusions
    
    * add code smell
    
    * remove qualitygate

--- services/demo2/demo2/main.py
+++ services/demo2/demo2/main.py
@@ -1,1 +1,22 @@
-print("hello world!")
+def too_many_parameters(param1, param2, param3, param4, param5, param6, param7):
+    ...
+
+
+def calcular_area_cuadrado(lado):
+    return lado * lado
+
+def calcular_perimetro_cuadrado(lado):
+    return 4 * lado
+
+def calcular_area_rectangulo(base, altura):
+    return base * altura
+
+def calcular_perimetro_rectangulo(base, altura):
+    return 2 * (base + altura)
+
+def unused_variable():
+    a = 10
+    # 'a' no se utiliza en el resto del código
+    return 5
+
+print("hello world!!")
